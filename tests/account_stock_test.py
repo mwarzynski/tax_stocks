93a4807d488b70c18c7738413a4e8d3eb71f4839
Paweł Zarembski <pawezarem@gmail.com>
2023-03-30 23:06:21 +0200

    calculate ratio

--- tests/account_stock_test.py
+++ tests/account_stock_test.py
@@ -248,6 +248,61 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(account.get_profit(year=2021), 150)
         self.assertEqual(account.get_tax(year=2021), 28.5)
 
+    def test_evaluate_stock_split(self):
+        exchange = ExchangeMock()
+        account = Account(exchange)
+
+        # Buy 1 TSLA for 100 PLN.
+        account.do_transaction(
+            Transaction(
+                trade_date=datetime(2021, 1, 1),
+                settle_date=datetime(2021, 1, 1),
+                currency=Currency.PLN,
+                activity=Activity.BUY,
+                symbol="TSLA",
+                quantity=Decimal(1),
+                price=Decimal(100),
+                amount=Decimal(100),
+                dividend_tax_deducted=Decimal(0),
+            )
+        )
+
+        # Split stack with ratio 5
+        account.do_transaction(
+            Transaction(
+                trade_date=datetime(2021, 1, 2),
+                settle_date=datetime(2021, 1, 2),
+                currency=Currency.PLN,
+                activity=Activity.SSP,
+                symbol="TSLA",
+                quantity=Decimal(4),  # 1 + 4
+                price=Decimal(0),
+                amount=Decimal(0),
+                dividend_tax_deducted=Decimal(0),
+            )
+        )
+
+        # Sell 5 TSLA for 200 PLN.
+        account.do_transaction(
+            Transaction(
+                trade_date=datetime(2021, 1, 3),
+                settle_date=datetime(2021, 1, 3),
+                currency=Currency.PLN,
+                activity=Activity.SELL,
+                symbol="TSLA",
+                quantity=Decimal(5),
+                price=Decimal(40),
+                amount=Decimal(200),
+                dividend_tax_deducted=Decimal(0),
+            )
+        )
+
+        # Compute profits and taxes.
+        # 200 PLN - 100 PLN = 100 PLN profit.
+        # 100 PLN * 19% = 19 PLN tax.
+        self.assertEqual(account.get_profit(year=2021), 100)
+        self.assertEqual(account.get_tax(year=2021), 19)
+
 
 if __name__ == "__main__":
     unittest.main()
